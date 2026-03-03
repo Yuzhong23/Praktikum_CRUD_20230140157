b3073e1285e07eb9512fd731e42e523b1ff05a1e
Yuzhong23 <fatursetia123@gmail.com>
2026-03-03 10:53:30 +0700

    menambahkan codenya

--- src/main/java/com/example/Prakatikum1/mapper/UserMapper.java
+++ src/main/java/com/example/Prakatikum1/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.example.Prakatikum1.mapper;
 
+import com.example.Prakatikum1.model.dto.UserDto;
+import com.example.Prakatikum1.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+    UserDto toUserDtoData (User user);
 }
--- src/main/java/com/example/Prakatikum1/service/UserService.java
+++ src/main/java/com/example/Prakatikum1/service/UserService.java
@@ -1,4 +1,14 @@
 package com.example.Prakatikum1.service;
 
+import com.example.Prakatikum1.model.dto.UserAddRequest;
+import com.example.Prakatikum1.model.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto AddUser(UserAddRequest request);
+    List<UserDto> getAllUser();
+    UserDto getUserById(String id);
+    UserDto UpdateUser(String id, UserAddRequest request);
+    void DeleteUser(String id);
 }
